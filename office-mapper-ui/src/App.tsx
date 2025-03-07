ab4a3a555d93cc799d9035dfd6fddb3cc2445463
Josh Jiang <josh.jiang.dbf@gmail.com>
2025-03-06 23:31:36 -0500

    added test call to local api

--- office-mapper-ui/src/App.tsx
+++ office-mapper-ui/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        setError(error.message);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello who?</h1>
+      <p>Test call to API:</p>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>Error: {error}</p>}
     </>
   )
 }
